cd87c40012cf70560541eb72aec8ff6ae4a2e4fd
Haytham BEN AICHA <haythembenaicha@gmail.com>
2018-01-16 23:23:21 +0100

    + Setup project architecture

---
+++ GithubViewer/GithubViewer/Global/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GithubViewer
+//
+//  Created by Haythem BEN AICHA on 1/16/18.
+//  Copyright © 2018 Haythem BEN AICHA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
