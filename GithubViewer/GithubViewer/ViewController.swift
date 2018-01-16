872f8d7ba2cf3f0dfc3cb263d1242be0633229f7
Haytham BEN AICHA <haythembenaicha@gmail.com>
2018-01-16 01:07:26 +0100

    + Project Creation

---
+++ GithubViewer/GithubViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubViewer
+//
+//  Created by Haythem BEN AICHA on 1/16/18.
+//  Copyright © 2018 Haythem BEN AICHA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
